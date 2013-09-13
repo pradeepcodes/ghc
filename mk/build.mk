a6b8b8e338b11cca155588a9c2383c2d7350254b
Pradeep Roark <pradeep.roark@gmail.com>
2013-09-13 12:05:06 +0100

    force add build.mk

---
+++ mk/build.mk
@@ -0,0 +1,9 @@
+HADDOCK_DOCS         = NO
+BUILD_DOCBOOK_HTML   = NO
+BUILD_DOCBOOK_PS     = NO
+BUILD_DOCBOOK_PDF    = NO
+SPLIT_OBJS           = NO
+INTEGER_LIBRARY      = integer-simple
+Stage1Only 	     = YES
+DYNAMIC_GHC_PROGRAMS = NO
+GhcLibWays           = v
