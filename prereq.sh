0acac7c6cf294008aa0a7d7c36ee684cdb3ca2a0
Pradeep Roark <pradeep.roark@gmail.com>
2013-09-13 10:59:32 +0100

    remove docbook dep. to reduce build time. install terminfo

--- prereq.sh
+++ prereq.sh
@@ -2,7 +2,6 @@ echo $PATH
 pwd
 brew update
 brew install autoconf automake
-brew install docbook
 
 #install llvm
 wget llvm.org/releases/3.0/clang+llvm-3.0-x86_64-apple-darwin11.tar.gz && tar xvfz clang+llvm-3.0-x86_64-apple-darwin11.tar.gz && sudo mv clang+llvm-3.0-x86_64-apple-darwin11 /usr/local/llvm
@@ -10,6 +9,10 @@ wget llvm.org/releases/3.0/clang+llvm-3.0-x86_64-apple-darwin11.tar.gz && tar xv
 
 #install haskell-platform
 wget lambda.haskell.org/platform/download/2013.2.0.0/Haskell%20Platform%202013.2.0.0%2064bit.pkg && sudo installer -pkg "Haskell Platform 2013.2.0.0 64bit.pkg" -target /
+
+#update cabal and install any other deps
+cabal update
+cabal install terminfo
 
 #get ghc ios wrapper scripts
 sudo git clone https://github.com/ghc-ios/ghc-ios-scripts.git /usr/local/ghc-ios-scripts
