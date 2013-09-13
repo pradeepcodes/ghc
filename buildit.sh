5b5804c7b64bcc3983f08c1154ca88e1f2134d8f
Pradeep Roark <pradeep.roark@gmail.com>
2013-09-13 09:53:54 +0100

    build ghc ios sim

--- buildit.sh
+++ buildit.sh
@@ -1,4 +1,7 @@
 echo $PATH
 ./sync-all --no-dph get
 perl boot
-./configure
+./configure --target=i386-apple-darwin11 --prefix=/usr/local/ghc-ios-sim/
+make
+sudo mkdir -p /usr/local/ghc-ios-sim/
+sudo make install
