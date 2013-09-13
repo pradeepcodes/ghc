6c417a94d407906ed4dbc7a454818424549fa8c1
Pradeep Roark <pradeep.roark@gmail.com>
2013-09-13 09:58:19 +0100

    git clone ghc ios scripts and add to path

--- prereq.sh
+++ prereq.sh
@@ -10,3 +10,6 @@ wget llvm.org/releases/3.0/clang+llvm-3.0-x86_64-apple-darwin11.tar.gz && tar xv
 
 #install haskell-platform
 wget lambda.haskell.org/platform/download/2013.2.0.0/Haskell%20Platform%202013.2.0.0%2064bit.pkg && sudo installer -pkg "Haskell Platform 2013.2.0.0 64bit.pkg" -target /
+
+#get ghc ios wrapper scripts
+sudo git clone https://github.com/ghc-ios/ghc-ios-scripts.git /usr/local/ghc-ios-scripts
