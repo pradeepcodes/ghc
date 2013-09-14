e226dca41185059189f050ddb3f67ee1df76d043
Pradeep Roark <pradeep.roark@gmail.com>
2013-09-14 07:46:37 +0100

    increase make job

--- buildit.sh
+++ buildit.sh
@@ -2,6 +2,6 @@ echo $PATH
 ./sync-all --no-dph get
 perl boot
 ./configure --target=arm-apple-darwin10 --prefix=/usr/local/ghc-ios/
-make
+make -j5
 sudo mkdir -p /usr/local/ghc-ios/
 sudo make install
