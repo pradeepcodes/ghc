3b6e0a193478b890c6cf04ef263b3d9fb870d6cd
Pradeep Roark <pradeep.roark@gmail.com>
2013-09-14 17:37:56 +0100

    fix configure

--- buildit.sh
+++ buildit.sh
@@ -1,7 +1,7 @@
 echo $PATH
 ./sync-all --no-dph get
 perl boot
-./configure --target=arm-apple-darwin10 --prefix=/usr/local/ghc-ios-sim/
+./configure --target=i386-apple-darwin11 --prefix=/usr/local/ghc-ios-sim/ --with-gcc=i386-apple-darwin11-gcc
 make -j5
 sudo mkdir -p /usr/local/ghc-ios-sim/
 sudo make install
